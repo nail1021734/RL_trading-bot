Fix: Remove bugs in PPO algorithm and environment. 1. Revise the wrong calculation formula when computing loss. 2. Fix the bug when environment using softmax.
feat: Add new feature in PPO algorithm.
1. Add GAE.

On branch master
Changes to be committed:
	modified:   .gitignore
	modified:   PPO.py
	new file:   auto_run_script.py
	modified:   config.py
	modified:   envs.py
	new file:   openAI_env.py
	modified:   train_M.py
	modified:   utils.py

PPO.py
@@ -1,8 +1,29 @@
 import torch
 from torch.distributions import MultivariateNormal, Categorical
 import numpy as np
+from typing import Union, Dict, List
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class FixedVariance:
+    def __init__(
+        self,
+        init_std: float,
+        decay_rate: float,
+        min_value: float,
+        decay_episode: int,
+    ):
+        self.var = init_std**2
+        self.decay_rate = decay_rate
+        self.min_value = min_value
+        self.decay_episode = decay_episode
+
+    def step(self):
+        self.var = max(self.var * (self.decay_rate**2), self.min_value)
+
+    def get_var(self):
+        return self.var
+
 
 class RolloutBuffer:
     def __init__(self):
@@ -27,17 +48,15 @@ class ActorCritic(torch.nn.Module):
         state_dim: int,
         action_dim: int,
         hidden_dim: int,
+        tanh_action: bool,
         has_continuous_action_space: bool,
-        action_std_init: float,
-        trainable_std = False,
+        fix_var: Union[FixedVariance, None] = None,
     ):
         super(ActorCritic, self).__init__()
         self.has_continuous_action_space = has_continuous_action_space
-        self.trainable_std = trainable_std
-
+        self.tanh_action = tanh_action
         self.action_dim = action_dim
-        if has_continuous_action_space and trainable_std == False:
-            self.action_var = torch.full((action_dim,), action_std_init * action_std_init).to(device)
+        self.fix_var = fix_var
 
         if has_continuous_action_space:
             self.actor = torch.nn.Sequential(
@@ -45,8 +64,7 @@ class ActorCritic(torch.nn.Module):
                 torch.nn.ReLU(),
                 torch.nn.Linear(hidden_dim, hidden_dim),
                 torch.nn.ReLU(),
-                torch.nn.Linear(hidden_dim, action_dim),
-                torch.nn.Tanh(),
+                torch.nn.Linear(hidden_dim, action_dim*2) if fix_var is None else torch.nn.Linear(hidden_dim, action_dim),
             )
         else:
             self.actor = torch.nn.Sequential(
@@ -66,30 +84,26 @@ class ActorCritic(torch.nn.Module):
             torch.nn.Linear(hidden_dim, 1),
         )
 
-        if has_continuous_action_space and trainable_std == True:
-            self.trainable_cov_mat = torch.nn.Parameter(torch.ones(action_dim) * (action_std_init * action_std_init))
-            self.action_var = self.trainable_cov_mat.to(device)
-            # Why need retain_grad()?
-            self.action_var.retain_grad()
-
-    def set_action_std(self, new_action_std: float):
-        if self.has_continuous_action_space:
-            self.action_var = torch.full(
-                (self.action_dim,),
-                new_action_std * new_action_std
-            ).to(device)
-
     def forward(self):
         raise NotImplementedError
 
     def act(self, state: torch.Tensor):
         if self.has_continuous_action_space:
-            action_mean = self.actor(state)
-            if not self.trainable_std:
-                cov_mat = torch.diag_embed(self.action_var).unsqueeze(dim=0)
+            actor_pred = self.actor(state)
+            if self.fix_var is not None:
+                action_mean = actor_pred
+                if self.tanh_action:
+                    action_mean = torch.nn.functional.tanh(action_mean)
+                cov_mat = torch.diag_embed(torch.ones(self.action_dim) * self.fix_var.get_var()).to(device)
+                dist = MultivariateNormal(action_mean, cov_mat)
             else:
-                cov_mat = torch.diag_embed(self.action_var).unsqueeze(dim=0)
-            dist = MultivariateNormal(action_mean, cov_mat)
+                action_mean = actor_pred[:self.action_dim]
+                if self.tanh_action:
+                    action_mean = torch.nn.functional.tanh(action_mean)
+                    action_var = torch.sigmoid(actor_pred[self.action_dim:]) + 1e-5
+                else:
+                    action_var = torch.nn.functional.softplus(actor_pred[self.action_dim:]) + 1e-5
+                dist = MultivariateNormal(action_mean, torch.diag_embed(action_var))
         else:
             action_probs = self.actor(state)
             dist = Categorical(action_probs)
@@ -102,15 +116,27 @@ class ActorCritic(torch.nn.Module):
 
     def evaluate(self, state: torch.Tensor, action: torch.Tensor):
         if self.has_continuous_action_space:
-            action_mean = self.actor(state)
-            action_var = self.action_var.expand_as(action_mean)
-            if not self.trainable_std:
-                cov_mat = torch.diag_embed(action_var)
+            actor_pred = self.actor(state)
+            if self.fix_var is not None:
+                action_mean = actor_pred
+                if self.tanh_action:
+                    action_mean = torch.nn.functional.tanh(action_mean)
+
+                # Set co-variance of distribution.
+                action_var = torch.ones(self.action_dim) * self.fix_var.get_var()
+                action_var = action_var.expand_as(action_mean)
+                cov_mat = torch.diag_embed(action_var).to(device)
+                dist = MultivariateNormal(action_mean, cov_mat)
             else:
-                cov_mat = torch.diag_embed(action_var)
-            dist = MultivariateNormal(action_mean, cov_mat)
-            if self.action_dim == 1:
-                action = action.reshape(-1, self.action_dim)
+                action_mean = actor_pred[:, :self.action_dim]
+                if self.tanh_action:
+                    action_mean = torch.nn.functional.tanh(action_mean)
+                    action_var = torch.sigmoid(actor_pred[:, self.action_dim:]) + 1e-5
+                else:
+                    action_var = torch.nn.functional.softplus(actor_pred[:, self.action_dim:]) + 1e-5
+                dist = MultivariateNormal(action_mean, torch.diag_embed(action_var))
+                if self.action_dim == 1:
+                    action = action.reshape(-1, self.action_dim)
         else:
             action_probs = self.actor(state)
             dist = Categorical(action_probs)
@@ -134,67 +160,57 @@ class PPO:
         update_iteration: int,
         clip: float,
         has_continuous_action_space: bool,
-        action_std_init: float,
         vf_coef: float,
         entropy_coef: float,
-        trainable_std: bool = False,
+        tanh_action: bool,
+        use_GAE: bool,
+        fix_var_param: Union[Dict, None],
     ):
         self.has_continuous_action_space = has_continuous_action_space
-
-        if has_continuous_action_space:
-            self.action_std = action_std_init
-
         self.gamma = gamma
         self.clip = clip
         self.update_iteration = update_iteration
         self.buffer = RolloutBuffer()
         self.vf_coef = vf_coef
         self.entropy_coef = entropy_coef
-        self.trainable_std = trainable_std
+        self.tanh_action = tanh_action
+        self.use_GAE = use_GAE
+        if fix_var_param is None:
+            self.fix_var = None
+        else:
+            self.fix_var = FixedVariance(
+                init_std=fix_var_param['init_std'],
+                decay_rate=fix_var_param['decay_rate'],
+                min_value=fix_var_param['min_value'],
+                decay_episode=fix_var_param['decay_episode'],
+            )
 
         self.policy = ActorCritic(
             state_dim=state_dim,
             action_dim=action_dim,
             hidden_dim=hidden_dim,
+            tanh_action=tanh_action,
             has_continuous_action_space=has_continuous_action_space,
-            action_std_init=action_std_init,
-            trainable_std=trainable_std,
+            fix_var=self.fix_var,
         ).to(device)
 
         opt_list = [
             {'params': self.policy.actor.parameters(), 'lr': actor_learning_rate},
             {'params': self.policy.critic.parameters(), 'lr': critic_learning_rate},
         ]
-        if has_continuous_action_space and trainable_std:
-            opt_list.append(
-                {'params': self.policy.action_var, 'lr': actor_learning_rate}
-            )
         self.optimizer = torch.optim.Adam(opt_list)
 
         self.old_policy = ActorCritic(
             state_dim=state_dim,
             action_dim=action_dim,
             hidden_dim=hidden_dim,
+            tanh_action=tanh_action,
             has_continuous_action_space=has_continuous_action_space,
-            action_std_init=action_std_init,
-            trainable_std=trainable_std,
+            fix_var=self.fix_var,
         ).to(device)
         self.old_policy.load_state_dict(self.policy.state_dict())
 
         self.mse_loss = torch.nn.MSELoss()
-
-    def set_action_std(self, new_action_std: float):
-        if self.has_continuous_action_space:
-            self.action_std = new_action_std
-            self.policy.set_action_std(new_action_std)
-            self.old_policy.set_action_std(new_action_std)
-
-    def decay_action_std(self, action_std_decay_rate: float, min_action_std: float):
-        if self.has_continuous_action_space:
-            self.action_std = self.action_std - action_std_decay_rate
-            self.action_std = round(self.action_std, 4)
-            self.action_std = max(self.action_std, min_action_std)
-            self.set_action_std(self.action_std)
 
     def select_action(self, state: np.ndarray):
         with torch.no_grad():
@@ -218,16 +234,17 @@ class PPO:
             reversed(self.buffer.rewards),
             reversed(self.buffer.is_terminals),
         ):
-            # if is_terminal:
-                # discounted_reward = 0
-            # discounted_reward = reward + (self.gamma * discounted_reward)
-            # rewards.insert(0, discounted_reward)
-            rewards.insert(0, reward)
+            if self.use_GAE:
+                if is_terminal:
+                    discounted_reward = 0
+                discounted_reward = reward + (self.gamma * discounted_reward)
+                rewards.insert(0, discounted_reward)
+            else:
+                rewards.insert(0, reward)
         # print(sum(self.buffer.rewards))
 
         # Normalizing the rewards.
         rewards = torch.tensor(rewards, dtype=torch.float32).to(device)
-        rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-7)
 
         # Convert list to tensor.
         old_states = torch.squeeze(torch.stack(self.buffer.states, dim=0)).detach().to(device)
@@ -237,6 +254,7 @@ class PPO:
 
         # Calculate advantages
         advantages = rewards.detach() - old_state_values.detach()
+        advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-7)
         # Train policy with multiple epochs.
         for _ in range(self.update_iteration):
             # Evaluating old actions and values.
@@ -257,7 +275,7 @@ class PPO:
             self.optimizer.zero_grad()
             loss.mean().backward()
             # print(self.policy.action_var)
-            torch.nn.utils.clip_grad_norm_(self.policy.parameters(), 0.5)
+            torch.nn.utils.clip_grad_norm_(self.policy.parameters(), 0.2)
             self.optimizer.step()
 
         # Copy new weights into old policy.

envs.py
@@ -8,6 +8,7 @@ from datetime import datetime as dt
 from typing import Union, List, Dict, Tuple, Optional, Callable
 import math
 import yfinance as yf
+import torch
 yf.pdr_override()
 
 
@@ -206,8 +207,11 @@ class MultiFinanceEnv:
         initial_balance: int = 10000,
         episode_size: int = 30,
         initial_stock: int = 0,
+        softmax_action: bool = False,
         target_feature: str = 'Adj Close',
+        clip_action: bool = False,
         extra_feature_dict: Dict[str, Callable] = None,
+        use_final_reward: bool = False,
     ):
         r"""
         Initialize the environment.
@@ -221,6 +225,8 @@ class MultiFinanceEnv:
         else:
             self.end_date = dt.strptime(end_date, "%Y-%m-%d")
 
+        self.softmax_action = softmax_action
+        self.clip_action = clip_action
         self.tickers = tickers
         self.episode_size = episode_size
         self.initial_stock = initial_stock
@@ -230,9 +236,10 @@ class MultiFinanceEnv:
         self.state_feature_names = state_feature_names
         self.extra_feature_dict = extra_feature_dict
         # Features, now balance and now stock.
-        self.state_dim = 1 + (len(state_feature_names) + 1)*len(tickers)
+        self.state_dim = 1 + (len(state_feature_names) + 1)*len(tickers) + 1
         if extra_feature_dict is not None:
-            self.state_dim += len(extra_feature_dict)*len(tickers)
+            self.state_dim += (len(extra_feature_dict)-1)*len(tickers)
+        self.use_final_reward = use_final_reward
 
         # Get the data.
         self.data = pdr.get_data_yahoo(
@@ -248,7 +255,6 @@ class MultiFinanceEnv:
 
         # Initialize the state features.
         self.episode_data = None
-        self.episode_date = None
         self.begin_date = 0
         self.day = 0
         self.balance = initial_balance
@@ -283,6 +289,8 @@ class MultiFinanceEnv:
                     state_dict=state_dict,
                     state=state,
                     target_feature=self.target_feature,
+                    begin_date=self.begin_date,
+                    data_date=self.data_date,
                 )
 
         return np.array(state)
@@ -330,27 +338,68 @@ class MultiFinanceEnv:
         # Update the day.
         self.day = min(1 + self.day, self.episode_size)
 
-        # Update the balance and stock.
+        # Sell stock.
         for a, t in zip(action, self.tickers):
-            # Convert the action to a float between -1 and 1. (tanh)
-            # a = (math.exp(a) - math.exp(-a)) / (math.exp(a) + math.exp(-a))
-            # a *= 10
-            if a > 1:
-                a = 1
-            elif a < -1:
-                a = -1
+            if a >= 0:
+                continue
+            # Clip the action to a float between -1 and 1.
+            if self.clip_action:
+                if a < -1:
+                    a = -1
+                min_value = -self.stock[t]
+                max_value = 0.0
+                a = min_value + (max_value - min_value) * (a + 1) / 2
+
+            # Clip the action to the action bound.
+            if a < -self.stock[t]:
+                a = -self.stock[t]
+
+            a = int(a)
+
+            # Update balance and stock.
+            self.balance -= self.episode_data[t][self.target_feature][self.day-1] * a
+            self.stock[t] += a
+
+            # Calculate the reward.
+            current_portfolio = self.balance + self.stock[t] * self.episode_data[t][self.target_feature][self.day-1]
+            next_portfolio = self.balance + self.stock[t] * self.episode_data[t][self.target_feature][self.day]
+            total_reward += next_portfolio - current_portfolio
+
+        # Use softmax to calculate weight of each stock.
+        if self.softmax_action:
+            weight_action = torch.tensor(action)
+            weight_action /= weight_action.max()
+            weight_action[weight_action <= 0] = float('-inf')
+            weight_action = torch.nn.functional.softmax(weight_action, dim=-1)
+            weight_balance = weight_action * self.balance
+            weight_balance = weight_balance.tolist()
+            weighted_balance = {t: b for b, t in zip(weight_balance, self.tickers)}
+
+
+        # Buy stock.
+        for a, t in zip(action, self.tickers):
+            if a < 0:
+                continue
+            # Convert the action to a float between -1 and 1.
+            if self.clip_action:
+                if a > 1:
+                    a = 1
+                min_value = 0.0
+                if self.softmax_action:
+                    max_value = weighted_balance[t]//self.episode_data[t][self.target_feature][self.day-1]
+                else:
+                    max_value = self.balance//self.episode_data[t][self.target_feature][self.day-1]
+                a = min_value + (max_value - min_value) * (a + 1) / 2
 
             # Calculate the action bound.
-            action_bound = {
-                'min': -self.stock[t],
-                'max': self.balance//self.episode_data[t][self.target_feature][self.day-1]
-            }
-            # if a < action_bound['min']:
-                # a = action_bound['min']
-            # elif a > action_bound['max']:
-                # a = action_bound['max']
-
-            a = action_bound['min'] + (action_bound['max'] - action_bound['min'])/2*(a + 1)
+            if self.softmax_action:
+                bound = weighted_balance[t]//self.episode_data[t][self.target_feature][self.day-1]
+                if a > bound:
+                    a = bound
+            else:
+                bound = self.balance//self.episode_data[t][self.target_feature][self.day-1]
+                if a > bound:
+                    a = bound
 
             a = int(a)
 
@@ -365,6 +414,9 @@ class MultiFinanceEnv:
             # print(reward)
         # Return the next state and reward.
         next_state = self.get_state()
+
+        if done and self.use_final_reward:
+            total_reward = self.get_final_reward()
 
         return next_state, total_reward, done, {}
 

train_M.py
@@ -7,22 +7,31 @@ from torch.utils.tensorboard import SummaryWriter
 import numpy as np
 import random
 from eval_func import testing_model
-from utils import average, mov_average
+from utils import average, mov_average, date
 import gc
 
 
+
 if __name__ == '__main__':
+    # TODO:
+    # 1. Select and add more ticker.
+    # 2. Test GAE.
+    # 3. Add more feature.
+
+    stock_list = ['2330.TW', '2317.TW', '2454.TW', '2382.TW', '2412.TW', '2308.TW', '2881.TW', '6505.TW', '2882.TW', '2303.TW', '1303.TW', '1301.TW', '2886.TW', '3711.TW', '2891.TW', '2002.TW', '1216.TW', '5880.TW', '2207.TW', '2884.TW']
     # Create environment config.
     env_setting = {
         'start_date': '2010-01-01',
         'end_date': '2020-12-31',
-        'tickers': ['3231.TW', '2356.TW', '2610.TW', '2330.TW', '0050.TW'],
+        'tickers': stock_list,
         'state_feature_names': ['Open', 'Close', 'Adj Close'],
         'initial_balance': 10000,
         'initial_stock': 0,
         'episode_size': 30,
         'target_feature': 'Adj Close',
-        'extra_feature_dict': {'mov_avg': mov_average, 'avg': average,},
+        'clip_action': True,
+        'softmax_action': False,
+        'extra_feature_dict': {'mov_avg': mov_average, 'avg': average, 'date': date},
         # 'extra_feature_dict': None,
     }
 
@@ -30,13 +39,15 @@ if __name__ == '__main__':
     test_env_setting = {
         'start_date': '2020-12-31',
         'end_date': None,
-        'tickers': ['3231.TW', '2356.TW', '2610.TW', '2330.TW', '0050.TW'],
+        'tickers': stock_list,
         'state_feature_names': ['Open', 'Close', 'Adj Close'],
         'initial_balance': 10000,
         'initial_stock': 0,
         'episode_size': 30,
         'target_feature': 'Adj Close',
-        'extra_feature_dict': {'mov_avg': mov_average, 'avg': average,},
+        'clip_action': True,
+        'softmax_action': False,
+        'extra_feature_dict': {'mov_avg': mov_average, 'avg': average, 'date': date},
         # 'extra_feature_dict': None,
     }
 
@@ -51,27 +62,32 @@ if __name__ == '__main__':
         hidden_dim=256,
         actor_learning_rate=5e-4,
         critic_learning_rate=3e-4,
-        gamma=0.99,
+        gamma=0.925,
         update_iteration=10,
-        action_std_init=0.6,
         clip=0.2,
         vf_coef=0.5,
         entropy_coef=0.01,
         has_continuous_action_space=True,
-        trainable_std=False,
+        tanh_action=True,
+        use_GAE=True,
+        fix_var_param=None,
+        # fix_var_param={
+            # 'init_std': 0.6,
+            # 'decay_rate': 0.9999,
+            # 'min_value': 0.2,
+            # 'decay_episode': 1000,
+        # }
     )
 
     # Initialize training config.
     config = Config(
-        exp_name='exp4_both_avg_s22',
+        exp_name='baseline_tanh_date_GAE_newreward_test',
         model_config=model_config,
         # seed=42,
         seed=22,
-        training_timestep=50000,
+        training_episode_num=50000,
         # Update model every 10 episode.
         update_timestep=env.episode_size*10,
-        action_std_decay_timestep=env.episode_size*1000,
-        min_action_std=0.2,
         env_name='MultiFinanceEnv',
         env_kwargs=env_setting,
         test_env_kwargs=test_env_setting,
@@ -103,17 +119,18 @@ if __name__ == '__main__':
         update_iteration=model_config.update_iteration,
         clip=model_config.clip,
         has_continuous_action_space=model_config.has_continuous_action_space,
-        action_std_init=model_config.action_std_init,
         vf_coef=model_config.vf_coef,
         entropy_coef=model_config.entropy_coef,
-        trainable_std=model_config.trainable_std,
+        tanh_action=model_config.tanh_action,
+        use_GAE=model_config.use_GAE,
+        fix_var_param=model_config.fix_var_param,
     )
 
     # Training loop.
     timestep = 0
     # History rewards
     hst_final_rewards = []
-    for i_episode in range(config.training_timestep):
+    for i_episode in range(config.training_episode_num):
         state = env.reset()
         for _ in range(env.episode_size):
             timestep += 1
@@ -130,9 +147,14 @@ if __name__ == '__main__':
 
             # Update PPO agent.
             if timestep % config.update_timestep == 0 and timestep != 0:
+                if done:
+                    hst_final_rewards.append(env.get_final_reward())
                 print(
                     f'Episode: {i_episode}, timestep: {timestep}, Last_10_rewards_avg: {sum(hst_final_rewards)/len(hst_final_rewards):.2f}')
                 ppo_agent.update()
+
+            if ppo_agent.fix_var is not None and i_episode % ppo_agent.fix_var.decay_episode == 0 and i_episode != 0:
+                ppo_agent.fix_var.step()
 
             if timestep % config.log_timestep == 0 and timestep != 0:
                 print('Logging...')
@@ -161,14 +183,8 @@ if __name__ == '__main__':
                     f'checkpoint/{config.exp_name}/ppo_agent_{timestep}.pth'
                 )
 
-            if model_config.has_continuous_action_space and timestep % config.action_std_decay_timestep == 0 and timestep != 0 and not ppo_agent.trainable_std:
-                ppo_agent.decay_action_std(
-                    action_std_decay_rate=config.action_std_decay_timestep,
-                    min_action_std=config.min_action_std,
-                )
 
             if done:
-                hst_final_rewards.append(env.get_final_reward())
                 break
         gc.collect()
 